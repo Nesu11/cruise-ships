c80ae29886dd01e9f26f76ff7d6c61b79f2c80e1
Nesu11 <anesujane98@gmail.com>
2019-03-08 23:36:37 +0000

    different port/ship

--- src/ship.js
+++ src/ship.js
@@ -1,9 +1,14 @@
 
-function Ship(name, port) {
-  this.name = name 
-  this.port = port ;
+function Ship(currentPort,) {
+  this.currentPort = currentPort;
+}
+Ship.prototype.setSail = function() {
+this.currentPort = null
 }
 
+Ship.prototype.dock = function(port) {
+this.currentPort = port
+}
 
 
 module.exports = Ship ; 
